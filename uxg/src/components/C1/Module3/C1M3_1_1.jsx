4656e44620e7e0d144b7430645e0a29c4f23de55
mav <Maverickodes.mac@gmail.com>
2025-02-19 18:07:12 +0530

    Doing ux design thinking framework

--- uxg/src/components/C1/Module3/C1M3_1_1.jsx
+++ uxg/src/components/C1/Module3/C1M3_1_1.jsx
@@ -30,6 +30,68 @@ const C1M3_1_1 = () => {
                                 and observation sessions but also conduct
                                 research about the competitor's product and how
                                 people integrate the product in their life.
+                                Methods of Empathizing with the customer:
+                                <ol>
+                                    <li>
+                                        <p>
+                                            <b>Interviews</b>
+                                            <br />
+                                            They could be
+                                            Questionnaires/surveys, in-person
+                                            interviews, phone and video
+                                            interviews. Designers can then plan
+                                            interviews that achieve the best
+                                            balance between their information
+                                            needs and their project's
+                                            parameters.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            <b>Empathy Maps</b>
+                                            <br />
+                                            These are a great tool for
+                                            processing the information a
+                                            designer has collected. In a classic
+                                            empathy map, designers draw directly
+                                            from interviews to answer five
+                                            questions about their users:{" "}
+                                            <ul>
+                                                <li>
+                                                    who are the users and what
+                                                    are their situations
+                                                </li>
+                                                <li>
+                                                    what do users say about
+                                                    their experiences with
+                                                </li>
+                                                <li>
+                                                    what do your users think
+                                                    about
+                                                </li>
+                                                <li>
+                                                    what do your users think
+                                                    about before, after their
+                                                    experience
+                                                </li>
+                                                <li>
+                                                    what do users feel about
+                                                    their experience
+                                                </li>
+                                            </ul>
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            <b>Personas</b>
+                                            <br />
+                                            These are fictional characters who
+                                            represents user groups. They help
+                                            designers distill large amounts of
+                                            user info into managable chunks.
+                                        </p>
+                                    </li>
+                                </ol>
                             </p>
                         </li>
                         <li>
@@ -40,10 +102,191 @@ const C1M3_1_1 = () => {
                                 of the user problems are the most important to
                                 solve. You might also develop a value
                                 proposition that is a summary of why a customer
-                                would / should use the product you are designing.
+                                would / should use the product you are
+                                designing. Combination of tools used to answer
+                                this question
+                                <ul>
+                                    <li>
+                                        <p>
+                                            <b>User Stories</b>
+                                            <br />
+                                            User stories help the designr
+                                            understand the client requirements
+                                            better. It's a one sentence story
+                                            told from a persona pov. It should
+                                            encapsulate who the user is, what
+                                            they want to do, and why they want
+                                            to do it.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            <b>User Journeys</b>
+                                            <br />
+                                            Once a designer has distilled their
+                                            user groups into realistic personas
+                                            and stories, it's time to start
+                                            mapping out each persona's user
+                                            journey. A user journey is a series
+                                            of experiences that the user has as
+                                            they try to achieve their goal.
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            <b>Problem Statements</b>
+                                            <br />
+                                            With complete user journeys for each
+                                            persona, a designer can better
+                                            identify the problems their design
+                                            must solve or the needs their design
+                                            must address. Then, they can distill
+                                            these problems or needs into problem
+                                            statements. A problem statement
+                                            summarizes <b>who</b> the user is,{" "}
+                                            <b>what</b>
+                                            they need from a design, and{" "}
+                                            <b>why</b>.
+                                        </p>
+                                    </li>
+                                </ul>
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                <b>Ideate</b>
+                                <br />
+                                Goal of this stage is to come up with as many
+                                solutions as possible. Do not settle for your
+                                first solution because the most obvious solution
+                                is not always the greatest. Don't classify ideas
+                                as good or bad, just collect all the ideas. Keep
+                                this process judgement-free. You'll then analyze
+                                your potential solutions and start to make
+                                choices about which ones are the best options to
+                                pursue as prototypes. How ideation may progress:
+                                <ul>
+                                    <li>
+                                        <p>
+                                            <b>How Might We?</b>
+                                            <br />
+                                            Designers take the problem
+                                            statements in the define phase and
+                                            turn them into HMW questions. These
+                                            questions will reframe user problems
+                                            as exciting opportunities for
+                                            solutions.
+                                        </p>
+                                    </li>
+
+                                    <li>
+                                        <p>
+                                            <b>Rapid Sketching</b>
+                                            <br />
+                                            aka Crazy Eights is a visual
+                                            approach to ideation. It is useful
+                                            when designers need to brainstorm
+                                            the visual elements of the design.
+                                            <br />
+                                            Now, designers will take one of the
+                                            HMW statements and apply the
+                                            following:
+                                            <ul>
+                                                <li>
+                                                    <p>
+                                                        Divide a large sheet of
+                                                        paper into 8 squares
+                                                    </p>
+                                                </li>
+                                                <li>
+                                                    <p>
+                                                        Grab something to draw
+                                                        with
+                                                    </p>
+                                                </li>
+                                                <li>
+                                                    <p>Start an 8-min timer</p>
+                                                </li>
+                                                <li>
+                                                    <p>
+                                                        Take one minute to
+                                                        sketch an idea or
+                                                        solution in each square.
+                                                    </p>
+                                                </li>
+                                            </ul>
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            <b>Competitive Audits</b>
+                                            <br />
+                                            To solve a problem you can also
+                                            audit the competitors by asking
+                                            questions like, "what products do
+                                            they offer?", "what are the
+                                            products' strengths and areas of
+                                            improvements?"
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <p>
+                                            <b>SCAMPER</b>
+                                            <br />
+                                            Each letter of the acronym stands
+                                            for different actions:<br />
+                                            S - Substitute<br />
+                                            C - Combine<br />
+                                            A - Adapting<br />
+                                            M - Modify<br />
+                                            P - Put to another use<br />
+                                            E - Eliminate<br />
+                                            R - Rearrange<br />
+                                        </p>
+                                    </li>
+                                </ul>
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                <b>Prototype & Test</b>
+                                <br />
+                                <b>Prototype</b> phase is where the goal is to
+                                produce a product that demonstrates its
+                                functionality and can be used for testing.
+                                <b>Testing</b> phase is critical in developing
+                                the right solution to address user's problem and
+                                an organized approach of testing can help you
+                                create exceptional user experiences. Both these
+                                stages are interconnected and you will test each
+                                prototype you make. Take as much feedback of ech
+                                change that you make from the client. If you
+                                make a too polished version then you might not
+                                get as much as a reaction and feedback
+                                necessary.
                             </p>
                         </li>
                     </ol>
+                    When you are woking with businesses, be it a for-profit or
+                    non-profit, or a government organization, you will be given
+                    business requirements. They include two kinds of information
+                    : goals and parameters. <br />
+                    Goals include{" "}
+                    <ul>
+                        <li>the users they want to target</li>
+                        <li>outcomes they want the design to achieve</li>
+                        <li>their vision for the final look</li>
+                    </ul>
+                    Parameters include{" "}
+                    <ul>
+                        <li>Project budget, timeline and scope</li>
+                        <li>
+                            Specific tools, systems used to design the project
+                        </li>
+                        <li>
+                            the requirements and standards the design must meet
+                        </li>
+                    </ul>
                 </section>
             </div>
         </div>
